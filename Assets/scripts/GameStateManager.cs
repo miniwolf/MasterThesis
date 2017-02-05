8ee81ef8a032a5db93ca61427577bef34425fd8e
miniwolf <miniwolf1508@gmail.com>
2017-02-05 16:39:02 +0100

    Initial commit with unit tests, making the initial steps towards reading the xml of the stories we generate.

---
+++ Assets/scripts/GameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace Assets.scripts {
+    public class GameStateManager : MonoBehaviour {
+        private int idx = 0;
+
+        private void Start() {
+            Next();
+        }
+
+        public void Next() {
+            //var parser = new XMLParser();
+            var content = Load("Assets/story/story.xml");
+            //var LevelXML = parser.Parse(content);
+            Debug.Log(content);
+            idx++;
+        }
+
+        public static StoryContainer Load(string fileName) {
+            var serializer = new XmlSerializer(typeof(StoryContainer));
+            return (StoryContainer) serializer.Deserialize(new XmlTextReader(fileName));
+        }
+    }
+}
